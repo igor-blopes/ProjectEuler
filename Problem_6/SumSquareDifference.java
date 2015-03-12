b6b4278ee83a1326e46d9848fb3d547264f3cc4f
igor-blopes <igor-blopes@live.com>
2015-03-12 07:16:04 -0300

    Update SumSquareDifference.java
    add execute time

--- Problem_6/SumSquareDifference.java
+++ Problem_6/SumSquareDifference.java
@@ -2,6 +2,8 @@ package Problem_6;
 
 public class SumSquareDifference {
 	public static void main(String[] args) {
+		long start = System.currentTimeMillis();
+		
 		long sumSquareFirstHundred = 0;
 		long sumFirstHundredSquare = 0;
 		
@@ -11,6 +13,9 @@ public class SumSquareDifference {
 		}
 		
 		System.out.println((long)Math.pow(sumFirstHundredSquare, 2)-sumSquareFirstHundred);
+		
+		long finish = System.currentTimeMillis();
+		System.out.println("Run in: "+(finish-start)+"ms.");
 	}
 
 }
