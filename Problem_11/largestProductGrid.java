152a41bf1d577906792d80342e7c0698d3597914
igor-blopes <igor-blopes@live.com>
2015-03-12 07:18:18 -0300

    Update largestProductGrid.java
    add execute time

--- Problem_11/largestProductGrid.java
+++ Problem_11/largestProductGrid.java
@@ -3,6 +3,8 @@ package Problem_11;
 public class largestProductGrid {
 
 	public static void main(String[] args) {
+		long start = System.currentTimeMillis();
+		
 		int[][] grid = {{8, 2, 22, 97, 38, 15, 0, 40, 0, 75, 4, 5, 7, 78, 52, 12, 50, 77, 91, 8},
 						{49, 49, 99, 40, 17, 81, 18, 57, 60, 87, 17, 40, 98, 43, 69, 48, 4, 56, 62, 0},
 						{81, 49, 31, 73, 55, 79, 14, 29, 93, 71, 40, 67, 53, 88, 30, 03, 49, 13, 36, 65},
@@ -75,6 +77,9 @@ public class largestProductGrid {
 		
 		
 		System.out.println("Largest product on grid: "+largest);
+		
+		long finish = System.currentTimeMillis();
+		System.out.println("Run in: "+(finish-start)+"ms.");
 	}
 
 }
